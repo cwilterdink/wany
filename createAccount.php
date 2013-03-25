3b4bd70f6debe2b7bf3ff63ad276a17b3dd5579d
Patricia Lin <lin.patty@gmail.com>
2013-03-25 15:19:43 -0300

    Create createAccount.php

---
+++ createAccount.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>WANY</title>
+
+<link rel="stylesheet" type="text/css" media="all" href="main.css" />
+</head>
+
+
+<body>
+<div class="container">
+  <div class="nav">
+		<h1><a href="index.php">WANY</a></h1>
+	</div>
+	<div class="content">
+		<h3>Create Your WANY Account</h3>
+		
+		<form name="input" action="html_form_action.asp" method="get">
+			<p>First name: <input type="text" name="firstname"><br>
+			Last name: <input type="text" name="lastname"><br>
+			Email: <input type="text" name="email"><br>
+			Password:<input type="password" name="password"><br>
+			Re-enter Password: <input type="password" name="reenter"><br></p>
+			<input type="submit" value="Submit">
+		</form>
+		
+	</div>
+	
+</div>
+</body>
+</html>

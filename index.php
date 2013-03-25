4ccd533340ef3e82f4c491914e29fb5d2b0e0ba5
Patricia Lin <lin.patty@gmail.com>
2013-03-25 15:18:24 -0300

    Create index.php
    index

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<head>
+
+<title>WANY</title>
+
+<link rel="stylesheet" type="text/css" media="all" href="main.css" />
+
+
+</head>
+
+<div class="container">
+  <div class ="nav">
+		<a href="index.php"><h1>WANY</h1></a>
+		<div class ="login">
+			<form action="" method = "get">
+				<h2>
+				Username: <input type = "string" name = "user">
+				Password: <input type = "string" name = "pass">
+				<input type="submit" name="submit" value="Login">
+				</h2>
+			</form>
+			<a href="passwordRecovery.php" class="right"><h5>Forgot Password?</h5></a>
+
+		</div><!--login-->
+	</div><!--nav-->
+
+	
+	<div class ="sidePanel">
+	<br>
+		<center><h6>NEWS</h6><hr></center>
+		<p>We have nothing to say.</p>
+
+	</div><!--sidePanel-->
+
+	<div class ="content" id="feature">
+		
+
+	</div>
+
+
+</div><!--container-->
+
+
+</body>
+</html>

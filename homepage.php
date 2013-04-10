a3a82bf43f65c0e040b8043b2c05f45daf310aa5
Patricia Lin <lin.patty@gmail.com>
2013-04-10 17:18:11 -0300

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,40 +1,78 @@
 <!DOCTYPE html>
 <html>
-
-  <?php
-    $txt="WANY User Homepage";
-  ?>
-
-  <div class="navbar">
-    <div class="navbar-inner">
-      <a class="brand" href="#">Username</a>
-      <ul class="nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Settings</a></li>
-        <li><a href="#">Logout</a></li>
-      </ul>
-    </div>
-  </div>
-
   <head>
-    <link rel="stylesheet" href="/css/bootstrap.css">
-    <title><?php echo $txt; ?></title>
+    <title>WANY</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+  <style>
+      body {
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+    </style>
   </head>
 
+  
   <body>
-    <h1><img src="" alt="logo">WANY</h1>
+  
+  	<div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="index.php">WANY</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="index.php">Home</a></li>
+              <li><a href="#">Settings</a></li>
+              <li><a href="index.php">Logout</a></li>
+            </ul>
 
-  <form action="SearchResults.php" method="post">
-    Search: <input type="text" name="search"><br>
-    <input type="submit" value="Submit"><br>
-  </form>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+	<div class="container">
+		<h1><!--<img src="" alt="logo">-->WANY SEARCH</h1>
+		      <!-- Expiry-->
+		<div class="control-group">
+			<form action ="SearchResults.php"method="POST">
+			<label class="control-label" for="password">Budget
+				<div class="controls">
+					<select class="span1" name="budget" id="budget">
+						<option></option>
+						<option value="01">$0</option>
+						<option value="02">$1</option>
+						<option value="03">$5</option>
+						<option value="04">$10</option>
+						<option value="05">$15</option>
+						<option value="06">$20</option>
+						<option value="07">$25</option>
+						<option value="08">$30</option>
+						<option value="09">$35</option>
+						<option value="10">$40</option>
+						<option value="11">$45</option>
+						<option value="12">$50+</option>
+					</select>
+				<input class="span4" type="text" name="search">
+				<button type="submit" class="btn btn-success">Submit</button>
+				</label>
+				</div>
+			</form>
+      </div>
+
+
+
+	</div><!--/ .container-->
   
   <?php 
     $search = $_POST["search"];
     //Call database to search for this variable
   ?>
     
-  </body>
-</html>
+

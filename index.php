11df454c3ee75b80ae1812fd57d5d87ed8083ca7
takadas1 <takadas1@tcnj.edu>
2013-04-10 21:25:39 -0300

    Update index.php
    Took out the drop down bar for alpha testing

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
     <title>WANY</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 
-	<style>
+  <style>
       body {
         padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
       }
     </style>
   </head>
-  <body>
 
-	<div class="navbar navbar-fixed-top">
+  
+  <body>
+  
+  	<div class="navbar navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
           <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
@@ -26,33 +28,69 @@
           <div class="nav-collapse collapse">
             <ul class="nav">
               <li class="active"><a href="index.php">Home</a></li>
-              <li><a href="createAccount.php">Create Account</a></li>
-              <li><a href="passwordRecovery.php">Forogot Password</a></li>
+              <li><a href="#">Settings</a></li>
+              <li><a href="index.php">Logout</a></li>
             </ul>
-			  <form action ="homepage.php" class="navbar-form pull-right">
-              <input class="span2" type="text" placeholder="Email">
-              <input class="span2" type="password" placeholder="Password">
-              <button type="submit" class="btn btn-primary">Sign in</button>
-            </form>
+
           </div><!--/.nav-collapse -->
         </div>
       </div>
-    </div><!--/ .navbar-->
+    </div>
+
+	<div class="container-fluid">
 	
-	<div class="container">
-		<div class="hero-unit">
-			<h1>Bored and on a Budget?</h1> 
-			<h2>WANY can help you!</h2>
-			<p><a href="createAccount.php" class="btn btn-primary btn-large">Sign Up Now! &raquo;</a></p>
-			<p>Enjoy life without breaking the bank.</p>
-		</div>
+		 <div class="row-fluid">
+			<div class="span2">
+				<div class="well sidebar-nav">
+					<ul class="nav nav-list">
+					<li class="nav-header">User Profile</li>
 
-    </div> <!-- /container -->
+					<br><br><br><br><br><br><br> <!--potentially profile picture here-->
+					<li class="nav-header">Other</li>
+					<li><a href="#">Link</a></li>
+
+					</ul>
+				</div><!--/.well -->
+			</div><!--/span-->
+		
+		<div class="span10">
+          <div class="hero-unit">
+				<h1><!--<img src="" alt="logo">-->WANY SEARCH</h1>
+				<div class="control-group">
+					<form action ="SearchResults.php"method="POST">
+				<!--	<label class="control-label" for="password">Budget   -->
+						<div class="controls">
+						<!--	<select class="span2" name="budget" id="budget">
+								<option></option>
+								<option value="01">$0</option>
+								<option value="02">$1</option>
+								<option value="03">$5</option>
+								<option value="04">$10</option>
+								<option value="05">$15</option>
+								<option value="06">$20</option>
+								<option value="07">$25</option>
+								<option value="08">$30</option>
+								<option value="09">$35</option>
+								<option value="10">$40</option>
+								<option value="11">$45</option>
+								<option value="12">$50+</option>
+							</select>
+						-->
+						<input class="span6" type="text" name="search">
+						<button type="submit" class="btn btn-success">Submit</button>
+					</label>
+						</div>
+				</form>
+				</div><!--/control-group-->
+			</div><!--/hero-->
+		</div><!--/span-->
+	</div><!--/row-->
+		
 
 
 
-	<!------------------------------------------------------------------------->
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  </body>
+	</div><!--/ .container-->
+  
+  
+</body>   
 </html>
